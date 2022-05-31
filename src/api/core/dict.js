5f22ef4ba2434c821f74d08716332c4058451b52
lamesbond <1418190515@qq.com>
2022-05-31 22:01:18 +0800

    done

---
+++ src/api/core/dict.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+export default{
+  listByParentId(parentId) {
+    return request({
+      url: `/admin/core/dict/listByParentId/${parentId}`,
+      method: 'get'
+    })
+  }
+
+}
---
+++ src/views/core/integral-grade/list.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="app-container">
+    <!-- 表格 -->
+    <el-table :data="list" border stripe>
+      <el-table-column type="index" width="50" />
+      <el-table-column prop="borrowAmount" label="借款额度" />
+      <el-table-column prop="integralStart" label="积分区间开始" />
+      <el-table-column prop="integralEnd" label="积分区间结束" />
+      <el-table-column label="操作" >
+        <template slot-scope="scope">
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeById(scope.row.id)">删除</el-button>
+          <router-link :to="'/core/integral-grade/edit/' + scope.row.id" style="margin-right:5px;" >
+            <el-button type="primary" size="mini" icon="el-icon-edit"> 修改</el-button>
+          </router-link>
+        </template>
+      </el-table-column>
+
+    </el-table>
+  </div>
+</template>
+
+<script>
+import integralGradeApi from '@/api/core/integral-grade'
+export default {
+  name: "",
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      // 调用api
+      integralGradeApi.list().then(response => {
+        this.list = response.data.list
+      })
+    },
+
+    removeById(id) {
+      this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+          return integralGradeApi.removeById(id)
+        }).then(response => {
+          this.$message({
+            message: response.message,
+            type: 'success'
+          })
+          this.fetchData()
+        })
+        .catch(error => {
+          if (error === 'cancel') {
+            this.$message({
+              type: 'info',
+              message: '已取消删除'
+            })
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
